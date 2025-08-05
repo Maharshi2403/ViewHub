8b1fe6bd8efd59afea8f0a66d3a180586e3ac6fe
Maharshi Patel <102695974+Maharshi2403@users.noreply.github.com>
2025-08-05 13:04:23 -0400

    New start

---
+++ src/LandingPage/index.tsx
@@ -0,0 +1,15 @@
+
+
+function LandingPage(){
+     
+    return (
+       <>
+       <div>
+        <nav className="flex h-14 w-full bg-[#33323211] font-white text-xl justify-center item-center pr-7">Nav</nav>
+        
+       </div>
+       </> 
+    );
+}
+
+export default LandingPage;
